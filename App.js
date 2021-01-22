823e11f137d9b70c173cc06f6d5e6503fd7c4597
Luzzi Charly <luzzi.charly@gmail.com>
2021-01-22 16:03:45 +0100

    Init

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import {DeliverooHomePage, ProductPage} from './Screens';
+
+const App = () => {
+  return <ProductPage />;
+};
+
+export default App;
---
+++ Assets/TYPOGRAPHY.js
@@ -0,0 +1,25 @@
+import { COLORS } from "./COLORS";
+
+export const TYPOGRAPHY = {
+  M1: {
+    fontSize: 14,
+    color: "#fff",
+  },
+  M2: {
+    fontSize: 17,
+    fontWeight: "bold",
+    color: COLORS.FONT.black
+  },
+  M3: {
+    fontWeight: "200",
+    fontSize: 12,
+    color: COLORS.FONT.gray
+  },
+  M4: {
+    fontSize: 16,
+  },
+  H5: {
+    fontSize: 22,
+    fontWeight: 'bold'
+  }
+}
